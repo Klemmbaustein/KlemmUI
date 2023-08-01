f450e6b1d45429ab8d84b28a576e59fad4d99ec3
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2023-08-01 17:00:21 +0200

    fixes

--- Source/Math/MathHelpers.cpp
+++ Source/Math/MathHelpers.cpp
@@ -3,16 +3,14 @@
 
 bool Math::IsPointIn2DBox(Vector2f BoxA, Vector2f BoxB, Vector2f Point)
 {
-	bool InArea = false;
 	if (BoxA.X > BoxB.X)
 	{
-		InArea = (BoxA.X > Point.X && BoxA.Y > Point.Y && BoxB.X < Point.X&& BoxB.Y < Point.Y);
+		return (BoxA.X >= Point.X && BoxA.Y >= Point.Y && BoxB.X <= Point.X&& BoxB.Y <= Point.Y);
 	}
 	else
 	{
-		InArea = (BoxA.X < Point.X&& BoxA.Y < Point.Y&& BoxB.X > Point.X&& BoxB.Y > Point.Y);
+		return (BoxA.X <= Point.X && BoxA.Y <= Point.Y&& BoxB.X >= Point.X&& BoxB.Y >= Point.Y);
 	}
-	return InArea;
 }
 
 bool Math::NearlyEqual(float A, float B, float epsilon)

e8ee51abd80f554e6e54cc60d1598214c8d490bd
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2023-08-03 23:50:40 +0200

    setup fixes

--- Setup.ps1
+++ Setup.ps1
@@ -9,8 +9,9 @@ cp SDL\VisualC\SDL\x64\Release\SDL2.dll Examples\FileExplorer\SDL2.dll
 
 cd glew-cmake\build\vc15
 devenv.exe /upgrade glew_static.vcxproj
+echo "Upgrading glew_static.vcxproj"
 
-ping 127.0.0.1 10
+Start-Sleep -Seconds 10
 
 msbuild glew_static.vcxproj /p:Configuration=Release /p:Platform=x64
 msbuild glew_static.vcxproj /p:Configuration=Debug /p:Platform=x64

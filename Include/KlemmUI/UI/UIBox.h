8dbd450f064ff9d093874bdd29d5a881b8103340
Klemmbaustein <83748124+Legofan0807@users.noreply.github.com>
2023-09-20 21:06:46 +0200

    fixed a duplicate line

--- Include/KlemmUI/UI/UIBox.h
+++ Include/KlemmUI/UI/UIBox.h
@@ -66,7 +66,6 @@ public:
 	UIBox* SetPadding(double Up, double Down, double Left, double Right);
 	UIBox* SetPadding(double AllDirs);
 	UIBox* SetTryFill(bool NewTryFill);
-	UIBox* SetAlign(Align NewAlign);
 	UIBox* SetHorizontal(bool IsHorizontal);
 	bool GetTryFill();
 	virtual void OnChildClicked(int Index);

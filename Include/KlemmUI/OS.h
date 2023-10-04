ec3d9b4d6c3a1b8b163a93fb40ca6171ccb0ad40
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2023-10-04 20:37:06 +0200

    removed unused functions

--- Include/KlemmUI/OS.h
+++ Include/KlemmUI/OS.h
@@ -1,30 +1,7 @@
 #pragma once
 #include <string>
 
-/*
-* For the compilation to work on both Linux and Windows,
-* This header implements functions that are OS specific. This is
-* (hopefully) the only part of the code that would require to be modified for the
-* program to run on other OS's.
-*/
-
 namespace OS
 {
-	enum EConsoleColor
-	{
-		E_WHITE = 0,
-		E_GRAY = 1,
-		E_RED = 2,
-		E_GREEN = 3,
-		E_BLUE = 4,
-		E_YELLOW = 5
-	};
 	std::wstring Utf8ToWstring(std::string utf8);
-	void SetConsoleCanBeHidden(bool ConsoleCanBeHidden);
-	void SetConsoleWindowVisible(bool Visible);
-	std::string ShowOpenFileDialog();
-	std::string GetOSString();
-	void ClearConsoleWindow();
-	void SetConsoleColor(EConsoleColor NewColor);
-	void OpenFile(std::string Path);
 }

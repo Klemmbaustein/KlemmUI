a4d2269309b511cb856568db1ac7ec6879b335a0
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2024-01-29 15:23:29 +0100

    repo structure rework, part 2

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+declare -a dependencies=("Dependencies/SDL" "Dependencies/glew-cmake")
+
+for i in "${dependencies[@]}"; do
+	cd $i
+	cmake -S . -B bld/
+	cd bld/
+	make
+	cd ../../..
+done
+
+cp Dependencies/glew-cmake/bld/lib/libglew.a lib/Debug/
+cp Dependencies/glew-cmake/bld/lib/libglew.a lib/Release/
